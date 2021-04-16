4c9319327783b77bffe29dbc78c2c098a8602080
KimJitae <57979066+wlxo0401@users.noreply.github.com>
2021-04-16 17:36:25 +0900

    20210416
    20210416

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import cv2
 
 from PyQt5 import uic
 from PyQt5 import QtCore
@@ -17,17 +18,28 @@ class mainwindow(QMainWindow, form_class):
         self.setupUi(self)
         self.show()
         self.startsetting()
+        self.ui_action_ready()
 
     def startsetting(self):
         # 파일 종합 경로
-        self.file_path = os.path.join(os.environ["USERPROFILE"], "Desktop")
-        
+        self.video_file_path = os.path.join(os.environ["USERPROFILE"], "Desktop")
+        self.frame_file_path = os.path.join(os.environ["USERPROFILE"], "Desktop")
+        self.export_btn.setEnabled(False)
+        self.set_text("대기중")
+
+    def ui_action_ready(self):
         # 불러오기 버튼
         self.load_btn.clicked.connect(self.video_load)
         # 내보내기 버튼
         self.export_btn.clicked.connect(self.video_export)
-        self.export_btn.setEnabled(False)
-        self.setStext("대기중")
+
+        # 내보내기 버튼
+        self.frame_load_btn.clicked.connect(self.frame_load)
+        # 내보내기 버튼
+        self.frame_sum_btn.clicked.connect(self.video_export)
+
+
+        # self.info_btn.clicked.connect(self.info)
 
     def video_load(self):
         """
@@ -35,37 +47,47 @@ class mainwindow(QMainWindow, form_class):
         """
         # 필터를 이용하여 정해진 확장자만 선택이 가능하다.
         select_filter = "Image(*.mp4)"
-        self.file_path = QFileDialog.getOpenFileName(None, "파일 선택창", self.file_path, select_filter)[0]
+        self.video_file_path = QFileDialog.getOpenFileName(None, "파일 선택창", self.video_file_path, select_filter)[0]
         # 비디오 이름
         self.video_name = ""
         # 비디오 경로
         self.video_path = ""
-        if self.file_path:
+        if self.video_file_path:
             # 이미지 리스트에 사진 경로 담기
-            self.video_name = self.file_path.split("/")[-1]
+            self.video_name = self.video_file_path.split("/")[-1]
             self.name_lbl.setText(self.video_name)
-            temp = self.file_path.split("/")
+            temp = self.video_file_path.split("/")
             del temp[-1]
             for i in temp:
                 self.video_path += i + "/"
-            self.setStext("비디오 불러오기 완료")
+            self.set_text("비디오 불러오기 완료")
             self.export_btn.setEnabled(True)
 
-
     def video_export(self):
-        self.setStext("작업 시작")
+        self.set_text("작업 시작")
         self.load_btn.setEnabled(False)
         self.export_btn.setEnabled(False)
         # 폴더 생성
         self.createFolder(self.video_path, self.video_name.split(".")[0] + "_split")
         # 스플릿 시작
-        self.thread = Thread(self.file_path, self.folder, self.video_name.split(".")[0])
+        self.thread = Thread(self.video_file_path, self.folder, self.video_name.split(".")[0])
         # 피니쉬 신호 받기
-        self.thread.msg_sig.connect(self.setStext)
-        self.thread.end_sig.connect(self.eenndd)
+        self.thread.msg_sig.connect(self.set_text)
+        self.thread.end_sig.connect(self.thread_end)
         # 스타트
         self.thread.start()
 
+    def frame_load(self):
+        # 필터를 이용하여 정해진 확장자만 선택이 가능하다.
+        self.frame_file_path = QFileDialog.getExistingDirectory(None, "파일 선택창", self.frame_file_path)
+        self.frame_list = list()
+        if self.video_file_path:
+            for file_name in os.listdir(self.frame_file_path):
+                self.frame_list.append(os.path.join(self.frame_file_path))
+            print(len(frame_list))
+
+    def frame_export(self):
+        pass
 
     def createFolder(self, folder_path, folder_name):
         # 폴더 경로, 이름.  한번에 다받아서 진행해도됨
@@ -73,21 +95,20 @@ class mainwindow(QMainWindow, form_class):
         # 폴더 유무 확인하고 생성
         if not os.path.exists(self.folder):
             os.makedirs(self.folder)
-            self.setStext("폴더 생성 완료")
+            self.set_text("폴더 생성 완료")
         else:
-            self.setStext("폴더가 이미 존재합니다.")
+            self.set_text("폴더가 이미 존재합니다.")
             return "FILE_ALREADY"
 
-    def setStext(self, msg):
+    def set_text(self, msg):
         self.status_lbl.setText(msg)
 
-    def eenndd(self):
+    def thread_end(self):
         self.load_btn.setEnabled(True)
         self.export_btn.setEnabled(True)
 
 # 이름 변경용 쓰레드        
 class Thread(QThread):
-    """ 파일 저장용 스레드 """
     # 사용자 정의 시그널 선언
     msg_sig = QtCore.pyqtSignal(str)
     end_sig = QtCore.pyqtSignal(int)
@@ -135,3 +156,52 @@ class Thread(QThread):
 
         
 
+class Thread2(QThread):
+    msg_sig = QtCore.pyqtSignal(str)
+    end_sig = QtCore.pyqtSignal(int)
+
+    def __init__(self, original_folder: str = "", frame_path: str = "", completed_folder: str = "", media_name: str = "",debug=False):
+        QThread.__init__(self)
+        self.original_folder = original_folder
+        self.frame_path = frame_path
+        self.completed_folder = completed_folder
+        self.media_name = media_name
+
+    def run(self):
+        """
+        이미지를 영상으로 만들어 줍니다.
+
+        frame_path :
+        프레임을 영상으로 바꾸어 줍니다. 옵션들을 적용하느냐에 따라서 이미지를 불러오는 폴더는 변경이 됩니다.
+
+        completed_folder :
+        저장될 위치를 담을 변수입니다. 
+        """
+
+        # start default camera
+        video = cv2.VideoCapture(self.original_folder)
+
+        # find OpenCV version
+        major_ver = cv2.__version__.split('.')[0]
+
+        # 원본 영상의 프레임을 추출합니다.
+        if int(major_ver) < 3:
+            fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+        else:
+            fps = video.get(cv2.CAP_PROP_FPS)
+
+        # 서브 프로세서를 통해서 이미지를 영상으로 만듭니다.
+        
+        result = subprocess.Popen(f'ffmpeg -y -f image2 -r {fps} -i \"{frame_path}_%05d.png\" -vcodec libx264 \"{completed_folder + media_name}\"', stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+        if debug:
+            out, err = result.communicate()
+        else:
+            result.communicate()
+
+        exitcode = result.returncode
+
+
+        if exitcode != 0:
+            return "MEDIA_SUM_ERROR"
+        else:
+            return "MEDIA_SUM_COMPLETE"
